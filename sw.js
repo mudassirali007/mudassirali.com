f6d3a033942dc662c9827916158c6f4e95bebecb
mudassirali007 <mudassiraliansari@gmail.com>
2023-01-06 01:35:00 +0500

    made app PWA

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener("activate", event => {
     console.log("Service worker activated");
 });
 const chacheName = "mudassirali-PWA";
-const urlsToCache = ["./", "https://mudassirali007.github.io/mudassirali.com/manifest.json", "index.js", "icons/Logo.png", "icons/logo2.png", "icons/logoname.png", "font/flaticon.css", "style.css"]
+const urlsToCache = ["./", "manifest.json", "index.js", "icons/Logo.png", "icons/logo2.png", "icons/logoname.png", "font/flaticon.css", "style.css", "font/HelveticaNeueBold.ttf", "font/Flaticon.woff2"]
 
 self.addEventListener("install", (event) => {
     console.log("Service worker installed",self);
